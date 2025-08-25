dbe45abf78adbfd69bec46f697759f4cc180af5e
Usama Mansoor <usamamansoor79@gmail.com>
2025-08-25 17:26:42 +0500

    ready for deployment

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,7 @@ import Header from "./Header";
 import Hero from "./Hero";
 import FeaturedCars from "./FeaturedCars";
 import BookingForm from "./BookingForm";
-import Cta from "./cta";
+import Cta from "./Cta";
 import Testimonials from "./Testimonials";
 import SectionHeading from "./SectionHeading";
 import NeverMisDeal from "./NeverMisDeal";
